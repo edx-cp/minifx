33496a53b3a7d87ad077595211fafd20c5f6a8e4
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-09-01 11:08:22 +0100

    add test case

---
+++ src/test/java/edxcp/github/com/minifx/HelloTest.java
@@ -0,0 +1,22 @@
+package edxcp.github.com.minifx;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+    @Test
+    void test_Case01() {
+        Assertions.assertTrue(1==1);
+    }
+
+    @Test
+    void test_Case02() {
+        Assertions.assertTrue(1==1);
+    }
+
+//    @Test
+//    void test_Case03() {
+//        Assertions.assertTrue(1==2);
+//    }
+}
