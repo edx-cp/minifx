54ae254bcd189d29df438a26a0d5d3649e6daf02
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-09-14 14:23:52 +0100

    add other file

---
+++ src/main/java/edxcp/github/com/minifx/Test.java
@@ -0,0 +1,10 @@
+package edxcp.github.com.minifx;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("\n======================\nRunning the main method\n======================");
+    }
+}
+
+// COMMAND
+// mvn compile exec:java -Dexec.mainClass="edxcp.github.com.minifx.Test"
