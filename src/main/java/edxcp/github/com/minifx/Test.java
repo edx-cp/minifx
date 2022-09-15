e3593ef599b24a975989deeb79708a0bd6fbed76
Ratheil <ratheilesse@gmail.com>
2022-09-15 15:41:17 +0100

    Adding the dependence visualsearchtree

--- src/main/java/edxcp/github/com/minifx/Test.java
+++ src/main/java/edxcp/github/com/minifx/Test.java
@@ -1,8 +1,26 @@
 package edxcp.github.com.minifx;
 
-public class Test {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.uclouvain.visualsearchtree.tree.Tree;
+import org.uclouvain.visualsearchtree.tree.TreeVisual;
+import org.uclouvain.visualsearchtree.tree.Visualizer;
+
+public class Test extends Application {
     public static void main(String[] args) {
+        launch(args);
         System.out.println("\n======================\nRunning the main method\n======================");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Tree t = new Tree(0);
+        TreeVisual tv = new TreeVisual(()->{
+
+        },t,true);
+        t.createNode(1, 0,Tree.NodeType.INNER,()->{} , "info");
+        t.createNode(2, 1,Tree.NodeType.INNER,()->{} , "info");
+        Visualizer.show(tv);
     }
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module edxcp.github.com.minijavafx {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires org.uclouvain.visualsearchtree;
 
     opens edxcp.github.com.minifx to javafx.fxml;
     exports edxcp.github.com.minifx;
